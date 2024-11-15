b14bba96ccd0de1e827943dafe45cd8223cb3498
foric27 <forc27101992@gmail.com>
2024-11-15 19:47:13 +0300

    add u-boot-hi3516av100.patch

--- build.sh
+++ build.sh
@@ -1,23 +1,19 @@
-#!/bin/sh -e
+#!/bin/bash
 
-export ARCH=arm
-export CROSS_COMPILE=${CROSS_COMPILE:-arm-hisiv510-linux-}
+export CROSS_COMPILE=arm-linux-gnueabi-
+mkdir -p output
 
-OUTPUTDIR="${HOME}/src/hisi/uboot"
-SOCS="hi3516a hi3516d"
+for soc in hi3516a hi3516d; do
 
-for soc in ${SOCS};do
-
-sed -i s/hi3516.v100/${soc}v100/ include/configs/hi3516a.h
+sed -i "s|hi3516.v100|${soc}v100|" include/configs/hi3516a.h
 make clean
 make hi3516a_config
+
 cp reg_info_${soc}.bin .reg
-
 make -j8
-make mini-boot.bin
 
-cp mini-boot.bin ${OUTPUTDIR}/u-boot-${soc}v100-universal.bin
-#cp mini-boot.bin /srv/tftp/u-boot-${soc}v100-universal.bin
+make mini-boot.bin
+cp mini-boot.bin output/u-boot-${soc}v100-universal.bin
 
 done
 
